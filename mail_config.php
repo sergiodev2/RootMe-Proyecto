02069f401d135b5ad8138cd1103e9a3b67e854f9
SERGIO CASTILLA RONCEL <sergiocastillaroncel@gmail.com>
2026-05-09 09:59:46 +0200

    Delete mail_config.php

--- mail_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-define('SMTP_HOST', 'smtp.gmail.com');
-define('SMTP_PORT', 587);
-define('SMTP_USER', 'notificacionrootme@gmail.com');
-define('SMTP_PASS', 'ulkhhcaeadkqhaac');
-define('SMTP_FROM_EMAIL', 'notificacionrootme@gmail.com');
-define('SMTP_FROM_NAME', 'ROOT ME Academia');
